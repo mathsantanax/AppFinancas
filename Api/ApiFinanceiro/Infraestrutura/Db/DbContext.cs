e215c18623f54ce095ed548fad02d3d90e219e6d
Matheus Santana <matheus.santana7@hotmail.com>
2024-09-06 00:49:52 -0300

    começando projeto

---
+++ Api/ApiFinanceiro/Infraestrutura/Db/DbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiFinanceiro.Infraestrutura.Db
+{
+    public class DbContext //: DbContext
+    {
+
+    }
+}
